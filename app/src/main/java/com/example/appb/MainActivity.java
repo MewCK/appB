487e03328adda7b0885bbfcb27e6a07e855c186c
Miyuru <mckularathne@gmail.com>
2019-05-22 15:11:08 +0530

    hello

---
+++ app/src/main/java/com/example/appb/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.appb;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void buttonclick(View view) {
+        Intent intent;
+        intent = new Intent("com.example.myapplication.activitytwo");
+        startActivity(intent);
+    }
+}
